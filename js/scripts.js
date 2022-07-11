8adbf0240be0c80f18eace8e2b03417a252f9c64
Bmcgaughan <87346945+Bmcgaughan@users.noreply.github.com>
2022-07-11 10:58:07 -0400

    initial start

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+'use strict';
+
+
+const header = document.querySelector('.header');
+const nav = document.querySelector('.navbar');
+const navHeight = nav.getBoundingClientRect().height;
+const body = document.body;
